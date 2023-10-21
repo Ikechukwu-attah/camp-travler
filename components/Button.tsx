22e0f2bd6502840f667cfcf041a4eb30da993931
Ikechukwu-attah <attahikechukwu@gmail.com>
2023-10-21 18:12:54 -0400

    button component created

---
+++ components/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant: "btn_dark_green";
+};
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return <div>Button</div>;
+};
+
+export default Button;
