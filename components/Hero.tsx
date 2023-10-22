a0841b4aa9c623da5857569af7e7bdda71f91a77
Ikechukwu-attah <attahikechukwu@gmail.com>
2023-10-21 20:28:33 -0400

    hero section component completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section className="border-2 border-red-500 flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row ">
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row ">
       <div className="hero-map" />
       {/* //Left side */}
       <div className="relative z-20 flex flex-col flex-1 xl:w-1/2">
@@ -50,6 +50,31 @@ const Hero = () => {
             variant="btn_white_text"
             icon="/play.svg"
           />
+        </div>
+      </div>
+
+      {/* Right */}
+
+      <div className="relative flex flex-1 items-start ">
+        <div className=" relative z-20 w-[268px] flex flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+          <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20">Location</p>
+              <Image src="/close.svg" alt="close" width={24} height={24} />
+            </div>
+            <p className="bold-20 text-white">Aguas Calientes</p>
+          </div>
+          <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Distance</p>
+              <p className="bold-20 text-white">173.28 mi</p>
+            </div>
+
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Elevation</p>
+              <p className="bold-20 text-white">2.0 km</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
