42fdc91af52417fc787b5aae7e7ff30a46f69289
Ikechukwu-attah <attahikechukwu@gmail.com>
2023-10-22 18:01:46 -0400

    added people icons

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,7 +1,67 @@
+import { PEOPLE_URL } from "@/constants";
+import Image from "next/image";
 import React from "react";
 
+interface CampSiteInterface {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
+
+const CampSite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: CampSiteInterface) => {
+  return (
+    <div
+      className={`h-full w-full min-w-[110px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded lg:rounded-r-5xl 2xl:rounded-5xl`}
+    >
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10 border-2 border-blue-500">
+        <div className="flexCenter gap-4">
+          <div className="rounded-full bg-green-50 p-4">
+            <Image src="/folded-map.svg" alt="map" width={28} height={28} />
+          </div>
+          <div className="flex flex-col gap-1">
+            <h4 className="bold-18 text-white">{title}</h4>
+            <p className="regular-14 text-white">{subtitle}</p>
+          </div>
+        </div>
+
+        <div className="flexCenter gap-6">
+          <span className="flex -space-x-4 overflow-hidden ">
+            {PEOPLE_URL.map((url) => (
+              <Image
+                src={url}
+                key={url}
+                alt="person"
+                width={52}
+                height={52}
+                className="inline-block h-10 w-10 rounded-full"
+              />
+            ))}
+          </span>
+          <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 const Camp = () => {
-  return <div>Camp</div>;
+  return (
+    <section className="border-2 mt-44 border-green-500 2xl:max-container relative flex flex-col py-10 lg:bm-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <CampSite
+          backgroundImage="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Prigen, Pasuran"
+          peopleJoined="50+ Joined"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Camp;
