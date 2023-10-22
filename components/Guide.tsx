dca5d657b0750d1e9e392e4a926efb293e28ef75
Ikechukwu-attah <attahikechukwu@gmail.com>
2023-10-22 18:54:40 -0400

    added image to the guide component

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -22,7 +22,16 @@ const Guide = () => {
           </p>
         </div>
       </div>
-      Guide
+
+      <div className="flexCenter max-container relative w-full ">
+        <Image
+          src="/boat.png"
+          alt="boat"
+          width={1440}
+          height={580}
+          className="w-full object-cover object-center 2xl:rounded-5xl"
+        />
+      </div>
     </section>
   );
 };
