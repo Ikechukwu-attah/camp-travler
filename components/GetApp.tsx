b8620b65da1eb1a936aac55fde639c5f1f122b05
Ikechukwu-attah <attahikechukwu@gmail.com>
2023-10-23 07:22:07 -0400

    initiated getapp component

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Button from "./Button";
 
 const GetApp = () => {
-  return <div>GetApp</div>;
+  return (
+    <section className="flexCenter w-full flex-col pb-[100px]">
+      <div className="get-app">
+        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">
+            Get for free now
+          </h2>
+          <p className="regular-16 text-gray-10">
+            Available on iOS and Android
+          </p>
+          <div>
+            <Button
+              title="Download App"
+              type="button"
+              icon="/apple.svg"
+              full={true}
+              variant="btn_white"
+            />
+          </div>
+        </div>
+      </div>
+      GetApp
+    </section>
+  );
 };
 
 export default GetApp;
