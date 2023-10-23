cebf770d7e6a2780e8590ae1ea321ed71a10a187
Ikechukwu-attah <attahikechukwu@gmail.com>
2023-10-23 06:57:58 -0400

    completed feature component implementation

--- components/Features.tsx
+++ components/Features.tsx
@@ -8,7 +8,7 @@ const Features = () => {
       className="border-2 border-red-500 flex-col flexCenter overflow-hidden 
     bg-feature-bg bg-center bg-no-repeat py-24"
     >
-      <div className="max-container padding-container relative w-full justify-end">
+      <div className="max-container padding-container relative w-full flex justify-end">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image
             src="/phone.png"
@@ -30,7 +30,7 @@ const Features = () => {
             />
             <h2 className="bold-40 lg:bold-64">Our Features</h2>
           </div>
-          <ul>
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mt-20 lg:gap-20">
             {FEATURES.map((feature) => (
               <FeatureItem
                 key={feature.title}
@@ -56,11 +56,13 @@ type FeatureItem = {
 const FeatureItem = ({ title, icon, description, variant }: FeatureItem) => {
   return (
     <li className="flex w-full flex-col items-start">
-      <div className="rounded-full p-4 lg:p-7">
+      <div className="rounded-full p-4 lg:p-7 bg-green-50">
         <Image src={icon} alt="map" width={28} height={28} />
       </div>
-
-      {title}
+      <h2 className="bold-20 lg:bold-32 mt-5 capitalize"> {title}</h2>
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+        {description}
+      </p>
     </li>
   );
 };
